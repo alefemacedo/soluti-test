3a5683ae2b685b997410c67848d8d080262b8732
alefemacedo <almacedobr2@gmail.com>
2020-02-13 02:56:17 -0200

    Arquivo para insercao, recuperacao e remocao dos tokens no storage

---
+++ admin/src/utils/auth.js
@@ -0,0 +1,26 @@
+const TokenKey = "Admin-Token"
+const RefreshTokenKey = "Admin-Refresh-Token"
+
+export function getToken() {
+  return localStorage.getItem(TokenKey)
+}
+
+export function setToken(token) {
+  return localStorage.setItem(TokenKey, token)
+}
+
+export function removeToken() {
+  return localStorage.removeItem(TokenKey)
+}
+
+export function getRefreshToken() {
+  return localStorage.getItem(RefreshTokenKey)
+}
+
+export function setRefreshToken(refreshToken) {
+  return localStorage.setItem(RefreshTokenKey, refreshToken)
+}
+
+export function removeRefreshToken() {
+  return localStorage.removeItem(RefreshTokenKey)
+}
