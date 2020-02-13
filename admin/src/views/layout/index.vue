e817d1fabdfddb8c74d5cc9b251f656ba42147be
alefemacedo <almacedobr2@gmail.com>
2020-02-13 02:57:38 -0200

    Alteracao do layout para permitir logout

--- admin/src/views/layout/index.vue
+++ admin/src/views/layout/index.vue
@@ -17,25 +17,12 @@
           </b-navbar-nav>
 
           <b-navbar-nav class="ml-auto">
-            <b-nav-form>
-              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-            </b-nav-form>
-
-            <b-nav-item-dropdown text="Lang" right>
-              <b-dropdown-item href="#">EN</b-dropdown-item>
-              <b-dropdown-item href="#">ES</b-dropdown-item>
-              <b-dropdown-item href="#">RU</b-dropdown-item>
-              <b-dropdown-item href="#">FA</b-dropdown-item>
-            </b-nav-item-dropdown>
-
             <b-nav-item-dropdown right>
-              <!-- Using 'button-content' slot -->
               <template v-slot:button-content>
                 <em>User</em>
               </template>
               <b-dropdown-item href="#">Profile</b-dropdown-item>
-              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+              <b-dropdown-item @click="logout">Sign Out</b-dropdown-item>
             </b-nav-item-dropdown>
           </b-navbar-nav>
         </b-collapse>
@@ -52,7 +39,18 @@
 
 <script>
 export default {
-  name: "Layout"
+  name: "Layout",
+  methods: {
+    /**
+     * Desloga da aplicação
+     */
+    logout() {
+      this.$store.dispatch("LogOut")
+        .then(() => {
+          this.$router.push({ path: "/login" })
+        })
+    }
+  }
 }
 </script>
 
