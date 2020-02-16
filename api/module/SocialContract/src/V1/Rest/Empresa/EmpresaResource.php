a8b53687204b97fa5f3343121cca85596e579c22
alefemacedo <almacedobr2@gmail.com>
2020-02-16 14:42:55 -0300

    Alteracao do Resource de Empresa para permitir cadastro de empresas juntamente com os dados de seu Contrato Social

--- api/module/SocialContract/src/V1/Rest/Empresa/EmpresaResource.php
+++ api/module/SocialContract/src/V1/Rest/Empresa/EmpresaResource.php
@@ -2,19 +2,51 @@
 namespace SocialContract\V1\Rest\Empresa;
 
 use ZF\ApiProblem\ApiProblem;
+use ZF\ApiProblem\ApiProblemResponse;
 use ZF\Rest\AbstractResourceListener;
+use SocialContract\V1\Rest\Exception\UniqueConstraintViolationException;
+use SocialContract\V1\Rest\Exception\ValidationException;
 
-class EmpresaResource extends AbstractResourceListener
-{
+class EmpresaResource extends AbstractResourceListener {
+
+    protected $mapper;
+
+    public function __construct(EmpresaMapper $mapper) {
+        $this->mapper = $mapper;
+    }
+
     /**
      * Create a resource
      *
      * @param  mixed $data
      * @return ApiProblem|mixed
      */
-    public function create($data)
-    {
-        return new ApiProblem(405, 'The POST method has not been defined');
+    public function create($data) {
+        $return = [
+            'message' => ''
+        ];
+
+        try {
+            $this->mapper->create($data);
+            $return['message'] = 'Empresa cadastrada com sucesso';
+
+        } catch (UniqueConstraintViolationException $e) {
+            return new ApiProblemResponse(
+                new ApiProblem(422, 'Failed Validation', null, null, [
+                    'validation_messages' => json_decode($e->getMessage())
+                ])
+            );
+        } catch (ValidationException $e) {
+            return new ApiProblemResponse(
+                new ApiProblem(422, 'Failed Validation', null, null, [
+                    'validation_messages' => json_decode($e->getMessage())
+                ])
+            );
+        } catch (\Exception $e) {
+            return new ApiProblem(500, $e->getMessage());
+        }
+        
+        return $return;
     }
 
     /**
